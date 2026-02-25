8731e61c9ebe8f7c7c17c988afb7f064b7de4acb
Aryam <165002841+Aryamatallah@users.noreply.github.com>
2026-02-26 00:19:42 +0300

    Initial Commit

---
+++ APP1/APP1App.swift
@@ -0,0 +1,17 @@
+//
+//  APP1App.swift
+//  APP1
+//
+//  Created by Aryam on 26/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct APP1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
